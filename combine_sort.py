9f3511c917b3d69871e8e6bbe6f3e647d46daffa
Velda Kiara <veldakarimi@gmail.com>
2020-05-17 00:15:19 +0300

    sortin elements in a list

---
+++ combine_sort.py
@@ -0,0 +1,11 @@
+#The function should combine these two lists into one
+#  new list and sort the result.
+#  Return the new sorted list.
+
+
+def combine_sort(lst1, lst2):
+    unsorted = lst1 + lst2
+    sortedList = sorted(unsorted)
+    return sortedList
+
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
