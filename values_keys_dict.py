2fa729ca691f89e540016a49b68cc50c90a72500
Velda Kiara <veldakarimi@gmail.com>
2020-05-29 00:22:13 +0300

    added a file that gets values from keys in a dict

---
+++ values_keys_dict.py
@@ -0,0 +1,11 @@
+def values_that_are_keys(my_dictionary):
+  value_keys = []
+  for value in my_dictionary.values():
+    if value in my_dictionary:
+      value_keys.append(value)
+    return value_keys
+
+print(values_that_are_keys({1:100, 2:1, 3:4, 4:10}))
+# should print [1, 4]
+print(values_that_are_keys({"a":"apple", "b":"a", "c":100}))
+# should print ["a"]
