e7c44084db2a10de55a2e2d5323b6af871cf2f1e
Velda Kiara <veldakarimi@gmail.com>
2020-05-29 01:41:03 +0300

    unique values in a dict

---
+++ unique_values_dict.py
@@ -0,0 +1,14 @@
+#The function 
+# should return the number of unique values in the dictionary.
+
+def unique_values(my_dictionary):
+      seen_values = []
+      for value in my_dictionary.values():
+            if value not in seen_values:
+                seen_values.append(value)
+            return len(seen_values)
+
+print(unique_values({0:3, 1:1, 4:1, 5:3}))
+# should print 2
+print(unique_values({0:3, 1:3, 4:3, 5:3}))
+# should print 1
