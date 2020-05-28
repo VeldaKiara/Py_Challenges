0b31e54191bb26fe65cea6f4f20400d451d61380
Velda Kiara <veldakarimi@gmail.com>
2020-05-29 01:44:51 +0300

    indexing changes

--- unique_values_dict.py
+++ unique_values_dict.py
@@ -6,7 +6,7 @@ def unique_values(my_dictionary):
       for value in my_dictionary.values():
             if value not in seen_values:
                 seen_values.append(value)
-            return len(seen_values)
+      return len(seen_values)
 
 print(unique_values({0:3, 1:1, 4:1, 5:3}))
 # should print 2
